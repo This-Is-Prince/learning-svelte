97bd226cc0ea60727ab8f7d714af4f59d8aec9fa
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-12-31 20:10:37 +0530

    feat: hello world svelte first project complete

---
+++ hello-world/src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+<main>
+  <h1>Hello World</h1>
+</main>
+<style>
+  h1 {
+    color: rebeccapurple;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 2rem;
+  }
+</style>
